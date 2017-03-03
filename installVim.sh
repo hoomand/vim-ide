e39f200d4eb0d6c38c82f5caa4ff20752338bdb0
Bijan Hoomand <hoomand@gmail.com>
2017-03-03 16:52:05 +1100

    Change the install script for Mac

--- installVim.sh
+++ installVim.sh
@@ -25,15 +25,11 @@ fi
 
 ln -s $VIM_ROOT/vimrc $HOME/.vimrc
 
-echo -e "Installing vim (Vi Improved) package from repository"
-#installinv vim-gnome allows copy/pasting beween vim and system clipboard
-sudo apt-get --yes install vim vim-gnome
-
 echo -e "Installing ctags"
-sudo apt-get --yes install ctags
+brew install ctags
 
 echo -e "Installing cscope"
-sudo apt-get --yes install cscope
+brew install cscope
 
 echo -e "Initializing and checking out plugins submodules: "
 
@@ -44,42 +40,3 @@ git submodule init
 git submodule update 
 git submodule foreach git checkout master
 git submodule foreach git pull origin master
-
-echo -e "Installing vi_overlay" 
-
-while true; do
-    yn=y
-    #automatically answer 'yes' when 'y' is passed as first parameter
-    if [ "$1" != "y" ]; then 
-        read -p "Do you wish to install vi_overlay (sudo priviledges are required)? [y/n]: " yn
-    fi 
-    case $yn in 
-        [Yy]* ) echo -e "Yes selected";
-                if [ -f /usr/bin/vi.default ]; then 
-                    sudo rm /usr/bin/vi.default
-                fi
-                if [ -f /usr/bin/vim.default ]; then 
-                    sudo rm /usr/bin/vim.default
-                fi
-                sudo ln -s /etc/alternatives/vi /usr/bin/vi.default
-                sudo ln -s /etc/alternatives/vim /usr/bin/vim.default 
-                if [ -f /usr/bin/vi ]; then 
-                    sudo rm /usr/bin/vi 
-                fi 
-                if [ -f /usr/bin/vim ]; then 
-                    sudo rm /usr/bin/vim 
-                fi 
-                if [ -f $HOME/.vim/vimscripts/vi_overlay ]; then 
-                    sudo ln -s $VIM_ROOT/vimscripts/vi_overlay /usr/bin/vi
-                    sudo ln -s $VIM_ROOT/vimscripts/vi_overlay /usr/bin/vim 
-                    sudo chmod +x $VIM_ROOT/vimscripts/vi_overlay
-                else 
-                    echo -e "WARNING!!! $HOME/.vim/vimscripts/vi_overlay does not exist..."
-                fi
-                echo -e "vi_overlay installed.";
-                break;;
-        [Nn]* ) echo -e "No selected"; break;;
-        * ) echo -e "Please select yes or no";;
-    esac
-done
-
